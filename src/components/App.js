bfbd52b60b0f030cad2d9a02b6023661b754196e
atiks88 <141978852+atiks88@users.noreply.github.com>
2024-04-16 14:43:14 +0000

    my third commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react";
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+const App=() =>{
+  const [counter,setState]=useState(0);
+
+  const handleClick= ()=>{
+    setState(counter + 1);
+  }
+  
+    return (<>  <p>Button clicked {counter} times</p>
+    <button onClick={handleClick}>Click</button></>
+)
 }
 
 export default App
